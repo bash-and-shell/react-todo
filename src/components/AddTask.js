0228ff08e686e51fda6710f2b3d664d621518c82
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-28 17:00:44 +0100

    Add state to AddTask and submit function

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import './scss/AddTask.scss'
+import { useState } from 'react';
 
 const AddTask = (props) => {
+
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    props.onSubmit(value);
+  }
+
   return (
-    <Form className="add-task" onSubmit={props.onSubmit}>
+    <Form className="add-task" onSubmit={handleSubmit}>
               <Form.Label>Enter New Task</Form.Label>
 
       <Form.Group controlId="addTask" className='add-task-group'>
-        <Form.Control type="text" placeholder="Enter task" />
-        <Button className="searchforbutton" variant="primary" type="submit">Add</Button>
+        <Form.Control type="text" placeholder="Enter task" onChange={e => setValue(e.target.value)} value={value} />
+        <Button variant="primary" type="submit">Add</Button>
       </Form.Group>
     </Form>
   );
