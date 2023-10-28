55ca7a8efdce576bd5a1ba34ad845995483af5d8
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-28 15:21:28 +0100

    Setup task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,21 @@
 import './scss/Task.scss';
+import Form from 'react-bootstrap/Form';
 import { useState } from "react";
 
 const Task = () => {
-
+  return (
+    <Form>
+       {['checkbox'].map((type) => (
+        <div key={`default-${type}`} className="mb-3">
+          <Form.Check // prettier-ignore
+            type={type}
+            id={`default-${type}`}
+            label={`default ${type}`}
+          />
+        </div>
+      ))}
+    </Form>
+  );
 }
 
 export default Task;
