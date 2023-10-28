77f0ebba1e38149a6afab6daaeb3d8d212ff8619
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-28 16:08:55 +0100

    Basic task item with edit and delete button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,22 @@
 import './scss/Task.scss';
-import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { useState } from "react";
+import { Button } from 'react-bootstrap';
 
-const Task = () => {
-  return (
-    <Form>
-       {['checkbox'].map((type) => (
-        <div key={`default-${type}`} className="mb-3">
-          <Form.Check // prettier-ignore
-            type={type}
-            id={`default-${type}`}
-            label={`default ${type}`}
-          />
-        </div>
-      ))}
-    </Form>
-  );
+const Task = (props) => {
+
+  const {text, ...rest} = props;
+  return (<div className="task">
+ <ListGroup.Item className='task-text'>
+      {props.text}
+    </ListGroup.Item>
+    <span className="task-buttons">
+      <Button variant="light">Edit</Button>
+      <Button variant="danger">Delete</Button>
+    </span> 
+  </div>
+   
+  );  
 }
 
 export default Task;
