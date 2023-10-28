68bd7e032ccba823ac00482a240fbc502faffe4d
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-28 18:41:21 +0100

    Add basic add and delete tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer'
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('matches snapshot', () => {
+  const component = renderer.create(<App />);
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+test('adds element', () => {
+  render(<App />)
+
+  fireEvent.change(screen.getByPlaceholderText("Enter task"), { target: {value: 'hello'} })
+  fireEvent.click(screen.getByText("Add"))
+
+  expect(screen.getByText('hello'))
+})
+
+test('deletes element', () => {
+  render(<App />)
+
+  fireEvent.change(screen.getByPlaceholderText("Enter task"), { target: {value: 'hello'} })
+  fireEvent.click(screen.getByText("Add"))
+
+  const item = screen.getByText('hello')
+  expect(item)
+
+  fireEvent.click(screen.getByText("Delete"))
+
+  expect(item).not.toBeInTheDocument()
+})
+
