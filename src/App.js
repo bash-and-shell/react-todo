be199495aa6911990212dcf53bad642fe74e597f
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-28 17:00:09 +0100

    Begin mapping tasks on tasklist in main app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Container } from 'react-bootstrap';
+import AddTask from './components/AddTask.js';
+import "bootstrap/dist/css/bootstrap.css";
+import Task from './components/Task.js';
+import TaskList from './components/TaskList';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [tasks, setTasks] = useState(["Example Task 1"])
+  
+  const addTask = (task) => {
+    setTasks([...tasks, task])
+  }
+
+  const removeTask = (task) => {
+
+  }
+
+  const editTask = (task) => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container component="main" maxWidth="xs">
+      <AddTask onSubmit={addTask}/>
+      
+      <TaskList>
+        {tasks.map((task, index)=> {
+          return <Task index={index} text={task}/>
+        })}
+      </TaskList>
+      </Container>
+     
     </div>
   );
 }
