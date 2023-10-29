e331602573552d897d2d5f662b2ce83ccf4011f5
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-29 11:03:06 +0000

    NewList button/modal component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from './components/AddTask.js';
 import "bootstrap/dist/css/bootstrap.css";
 import Task from './components/Task.js';
 import TaskList from './components/TaskList';
+import NewList from './components/NewList';
 import { useState } from 'react';
 
 
@@ -11,6 +12,7 @@ const App = () => {
 
   const [tasks, setTasks] = useState([])
   const [tabKey, setTabKey] = useState('all')
+
   const addTask = (task) => {
     setTasks([...tasks, task])
   }
@@ -30,10 +32,15 @@ const App = () => {
     }
   }
 
+  const addList = () => {
+    
+  }
+
   return (
     <div className="App">
       <Container component="main" maxWidth="xs">
         <AddTask onSubmit={addTask} />
+        <NewList onSubmit={(e) => {console.log(e)}} />
         <Tabs
           id="tab-list"
           activeKey={tabKey}
---
+++ src/components/NewList.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+const NewList = (props) => {
+
+  const { onSubmit } = props;
+
+  const [show, setShow] = useState(false);
+  const [value, setValue] = useState('');
+  const handleClose = () => setShow(false);
+  const handleOpen = () => setShow(true);
+
+  const handleSubmit = () => {
+    setValue('')
+    onSubmit(value);
+  }
+
+
+  return(
+    <>
+      <Button variant='success' onClick={handleOpen}>
+        New List
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          Enter List Name
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="new-list-name">
+
+              <Form.Label>Enter name here</Form.Label>
+              <Form.Control autoFocus type="text" onChange={e => setValue(e.target.value)} value={value} />
+            </Form.Group>
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+        
+      </Modal>
+    </>
+  )
+}
+
+export default NewList;
