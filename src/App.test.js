8294a4792facf5573088a1c6e389d88bd7230b29
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-29 19:20:54 +0000

    Add tests for adding and deleting lists

--- src/App.test.js
+++ src/App.test.js
@@ -50,3 +50,49 @@ test('deletes element', () => {
 
 })
 
+test('adds list', () => {
+  render(<App 
+    task={[task]}
+    lists={['All', 'Test List 1']}
+    tabKey={'Test List 1'}
+    canChange={true} 
+ />)
+
+  fireEvent.click(screen.getByText("New List"))
+  fireEvent.change(screen.getByPlaceholderText("Enter name here"), { target: {value: 'Test List 2'} })
+
+  fireEvent.click(screen.getByText("Save Changes"))
+
+  expect(screen.queryByText('Test List 2')).toBeInTheDocument()
+})
+
+test('deletes list', () => {
+  render(<App 
+    task={[task]}
+    lists={['All', 'Test List 1']}
+    tabKey={'Test List 1'}
+    canChange={true} 
+ />)
+
+  fireEvent.click(screen.getByText("Delete List"))
+
+  fireEvent.click(screen.getAllByText("Delete")[2])
+
+  expect(screen.queryByText('Test List 1')).not.toBeInTheDocument()
+})
+
+test('edits list', () => {
+  render(<App 
+    task={[task]}
+    lists={['All', 'Test List 1']}
+    tabKey={'Test List 1'}
+    canChange={true} 
+ />)
+
+  fireEvent.click(screen.getByText("Edit List"))
+  fireEvent.change(screen.getByPlaceholderText("Enter name here"), { target: {value: 'Test List 2'} })
+
+  fireEvent.click(screen.getByText("Save Changes"))
+
+  expect(screen.queryAllByText('Test List 2')[0]).toBeInTheDocument()
+})
