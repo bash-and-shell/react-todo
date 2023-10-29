4a2a2b182d5b2c520742ca39ce93339f76973072
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-29 10:20:45 +0000

    Add checkbox's to strikethrough

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,24 @@
 import './scss/Task.scss';
+import { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 const Task = (props) => {
 
-  const {text, index, handleDelete, handleEdit, ...rest} = props;
+  const {text, index, handleDelete, handleEdit } = props;
+  const [checked, setChecked] = useState(false)
+
+  const handleCheck = () => {
+    setChecked(!checked)
+  }
+
+  // useEffect(() => {
+
+  // }, [checked])
 
   return (<ListGroup.Item key={index} className="task">
- <div className='task-text'>
-      {props.text}
-    </div>
+  <Form.Check  className={`task-text${checked ? ' strikethrough' : ''}`} type='checkbox' label={text} value={checked} onChange={(e) => {handleCheck()}} />
     <span className="task-buttons">
       <Button variant="light" onClick={() => handleEdit(index)}>Edit</Button>
       <Button variant="danger" onClick={() => handleDelete(index)}>Delete</Button>
