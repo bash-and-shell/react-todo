2444f7c2fd60869702be1f2936cce303ace20d4d
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-29 14:31:20 +0000

    Add editlist and deletelist buttons

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import AddTask from './components/AddTask.js';
 import "bootstrap/dist/css/bootstrap.css";
 import Task from './components/Task.js';
 import TaskList from './components/TaskList';
-import NewList from './components/NewList';
-import { useState } from 'react';
+import EditLists from './components/EditLists';
+import { useEffect, useState } from 'react';
 
 
 const App = () => {
@@ -13,6 +13,17 @@ const App = () => {
   const [tasks, setTasks] = useState([])
   const [lists, setLists] = useState(['All'])
   const [tabKey, setTabKey] = useState(lists[0])
+  const [canChangeLists, setCanChangeLists] = useState(false)
+
+  useEffect(() => {
+    if(lists.length > 1)
+      setCanChangeLists(true)
+  }, [lists])
+
+  useEffect(() => {
+    if(tabKey === 'All')
+      setCanChangeLists(false)
+  }, [tabKey])
 
   const completeItem = (index) => {
     let newTasks = tasks
@@ -23,8 +34,6 @@ const App = () => {
     newTasks[index] = task
 
     setTasks(newTasks)
-
-    console.log(tasks)
   }
 
 
@@ -38,8 +47,6 @@ const App = () => {
 
     setTasks([...tasks, task])
   }
-
-  
 
   const deleteTask = (task) => {
     const updateTasks = [...tasks]
@@ -57,14 +64,57 @@ const App = () => {
   }
 
   const addList = (list) => {
+    if(lists.includes(list)) 
+      return false;
+    
     setLists([...lists, list])
   }
+
+  const editList = (newName) => {
+    if(lists.includes(newName)) 
+      return false;
+
+    let updateLists = lists;
+    let updateTasks = tasks;
+
+    updateTasks.forEach((task, index) =>{
+      if(task.list === tabKey) {
+        task.list = newName;
+      }
+    })
+
+    updateLists[updateLists.indexOf(tabKey)] = newName;
+   
+    setTasks(updateTasks)
+    setLists(updateLists)
+    setTabKey(newName)
+  }
+
+  const deleteList = () => {
+    let updateTasks = [];
+
+    tasks.forEach((task) =>{
+      if(task.list !== tabKey) {
+        updateTasks.push(task)
+      }
+    })
+    
+    let updateLists = lists
+    updateLists = updateLists.splice(updateLists.indexOf(tabKey), 1)
+
+    
+    setTasks(updateTasks)
+    setLists(updateLists)
+  }
+  
 
   return (
     <div className="App">
       <Container component="main" maxWidth="xs">
-        <AddTask onSubmit={addTask} />
-        <NewList onSubmit={list => addList(list)} />
+        <AddTask onSubmit={addTask} enabled={canChangeLists} />
+        <EditLists type="New" onSubmit={list => addList(list)} />
+        <EditLists type="Edit" onSubmit={list => editList(list)} />
+        <EditLists type="Delete" onSubmit={list => deleteList(list)} />
         <Tabs
           id="tab-list"
           activeKey={tabKey}
