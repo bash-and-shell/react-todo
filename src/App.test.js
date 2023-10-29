eeb3de3bd399c7ad655eea13beda9ad70f02345e
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-29 19:08:47 +0000

    Test tasks being added and deleted

--- src/App.test.js
+++ src/App.test.js
@@ -3,33 +3,50 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from './App';
 
+const task = {
+  id: 0,
+  list: 'Test List 1',
+  value: 'Test task 1',
+  completed: false
+}
+
 test('matches snapshot', () => {
-  const component = renderer.create(<App />);
+
+  const component = renderer.create(<App 
+     task={[task]}
+     lists={['All', 'Test List 1']}
+     tabKey={'Test List 1'}
+     canChange={true} 
+  />);
 
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('adds element', () => {
-  render(<App />)
+  render(<App 
+    task={[task]}
+    lists={['All', 'Test List 1']}
+    tabKey={'Test List 1'}
+    canChange={true} 
+ />)
 
   fireEvent.change(screen.getByPlaceholderText("Enter task"), { target: {value: 'hello'} })
   fireEvent.click(screen.getByText("Add"))
 
-  expect(screen.getByText('hello'))
+  expect(screen.getAllByText('hello')[0]).toBeInTheDocument()
 })
 
 test('deletes element', () => {
-  render(<App />)
+  render(<App 
+    task={[task]}
+    lists={['All', 'Test List 1']}
+    tabKey={'Test List 1'}
+    canChange={true} 
+ />)
 
-  fireEvent.change(screen.getByPlaceholderText("Enter task"), { target: {value: 'hello'} })
-  fireEvent.click(screen.getByText("Add"))
+  fireEvent.click(screen.getAllByText("Delete")[0])
+  expect(screen.queryByText('Test task 1')).not.toBeInTheDocument()
 
-  const item = screen.getByText('hello')
-  expect(item)
-
-  fireEvent.click(screen.getByText("Delete"))
-
-  expect(item).not.toBeInTheDocument()
 })
 
