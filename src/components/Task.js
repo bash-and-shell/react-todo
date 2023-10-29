05e3d73963035796328b52848c509dd7dcae22ff
Zach Thomas <sc18zt@leeds.ac.uk>
2023-10-29 16:04:29 +0000

    Add list to task when in all tab

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,7 +6,7 @@ import { CheckCircle, CheckCircleFill } from 'react-bootstrap-icons';
 
 const Task = (props) => {
 
-  const {text, id, handleDelete, handleEdit, completed, onCheck } = props;
+  const {text, id, handleDelete, handleEdit, completed, onCheck,list, withList } = props;
   const [checked, setChecked] = useState(completed)
 
   const handleCheck = () => {
@@ -21,6 +21,8 @@ const Task = (props) => {
   return (<ListGroup.Item key={id} className="task">
     {checked ?  <CheckCircleFill size={25}  className="task-check-icon" onClick={() => handleCheck(id)}/> : <CheckCircle size={25} className="task-check-icon"  onClick={() => handleCheck(id)}/>}
     <label className={`task-text${checked ? ' strikethrough' : ''}`}>{text}</label>
+    {withList && <label className={`task-text list`}>{list}</label>}
+    <div className="task--spacer"/>
     <span className="task-buttons">
       <Button variant="light" onClick={() => handleEdit(id)}>Edit</Button>
       <Button variant="danger" onClick={() => handleDelete(id)}>Delete</Button>
